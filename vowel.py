163f06cc0befb0ea0633e276986f5a680bb8b317
Vignesh-254 <45882034+Vignesh-254@users.noreply.github.com>
2019-02-27 11:15:22 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,7 @@
 a=input()
 if a=='a' or a=='e' or a=='i' or a=='o' or a=='u':
-print('Vowel')
+  print('Vowel')
+elif a>=97 and a<=122:
+  print('Consonant')
 else:
-print('Invalid')
+  print('Invalid')
